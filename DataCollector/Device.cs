de86cbb6839df6205706bdf5624d8b3c4e893739
Enes Karagic <ekaragic@outlook.com>
2018-04-12 18:21:44 -0400

    Added device class with instructions. Added MeasureLengthDevice class

---
+++ DataCollector/Device.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    public class Device
+    {
+        //This method will return a random integer between 1 and 10 as a measurement of some imaginary object.
+        int GetMeasurement()
+        {
+            return 1;
+        }
+
+
+        //Add Timer
+        //Add another method that is going to be called by the timer
+        //Dispach timer page(682)
+    }
+}
