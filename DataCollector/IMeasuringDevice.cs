e98eaeb7164d97060b00a266783063e00e16843a
Enes Karagic <ekaragic@outlook.com>
2018-04-12 17:55:29 -0400

    Adding initial project files

---
+++ DataCollector/IMeasuringDevice.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataCollector
+{
+    interface IMeasuringDevice
+    {
+        /// <summary>
+        /// Public method declarations
+        /// </summary>
+        ///
+
+        //This method will return a decimal that represents the metric value of the most recent measurement that was captured.
+        decimal MetricValue();
+
+        //This method will return a decimal that represents the imperial value of the most recent measurement that was captured.
+        decimal ImperialValue();
+
+        //This method will start the device running. It will begin collecting measurements and record them.
+        void StartCollecting();
+
+        //This method will stop the device. It will cease collecting measurements.
+        void StopCollecting();
+
+        //This method will retrieve a copy of all of the recent data that the measuring device has captured. 
+        //The data will be returned as an array of integer values.
+        int[] GetRawData();
+
+    }
+}
