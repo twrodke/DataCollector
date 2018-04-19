657e810b44850a8c153782af7479ec27f888d2c0
Enes Karagic <ekaragic@outlook.com>
2018-04-19 18:38:59 -0400

    Added ui controls. Added code for measure length device class

--- DataCollector/MeasureLengthDevice.cs
+++ DataCollector/MeasureLengthDevice.cs
@@ -6,7 +6,40 @@ using System.Threading.Tasks;
 
 namespace DataCollector
 {
-    class MeasureLengthDevice// : IMeasuringDevice
+    class MeasureLengthDevice : IMeasuringDevice
     {
+        private string unitsToUse;
+        private int[] dataCaptured;
+        private int mostRecentMeasure;
+
+        MeasureLengthDevice()
+        {
+            
+        }
+
+        int[] IMeasuringDevice.GetRawData()
+        {
+            throw new NotImplementedException();
+        }
+
+        decimal IMeasuringDevice.ImperialValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        decimal IMeasuringDevice.MetricValue()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMeasuringDevice.StartCollecting()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IMeasuringDevice.StopCollecting()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
